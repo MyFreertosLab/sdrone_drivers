9e97ad815c45e5bf245d02606eebf3d0bddd9452
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-10-19 16:29:23 +0200

    Sviluppo telemetria

--- telemetry/include/telemetry.h
+++ telemetry/include/telemetry.h
@@ -7,8 +7,50 @@
 
 #ifndef COMPONENTS_DRIVERS_TELEMETRY_INCLUDE_TELEMETRY_H_
 #define COMPONENTS_DRIVERS_TELEMETRY_INCLUDE_TELEMETRY_H_
+#include <stdint.h>
+#include <esp_err.h>
+#include "esp_wifi.h"
+#include <esp_http_server.h>
 
+#define SDRONE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
 
-void wifi_init_softap(void);
+typedef struct {
+    httpd_handle_t hd;
+    int fd;
+} sdrone_telemetry_gs_socket_t;
+
+typedef struct {
+	int16_t rc_throttle;
+	int16_t rc_roll;
+	int16_t rc_pitch;
+	int16_t rc_yaw;
+	int16_t rc_aux1;
+	int16_t rc_aux2;
+	float roll;
+	float pitch;
+	float yaw;
+	float acc_x;
+	float acc_y;
+	float acc_z;
+	float w_x;
+	float w_y;
+	float w_z;
+	float w_thrust;
+	float ax_x;
+	float ax_y;
+	float ax_z;
+	float ax_thrust;
+} sdrone_telemetry_data_t;
+
+typedef struct {
+	httpd_handle_t server;
+	sdrone_telemetry_gs_socket_t sockets[SDRONE_MAX_STA_CONN];
+	sdrone_telemetry_data_t data;
+	uint8_t cursor;
+} sdrone_telemetry_state_t;
+typedef sdrone_telemetry_state_t* sdrone_telemetry_handle_t;
+
+void sdrone_telemetry_init(sdrone_telemetry_handle_t telemetry_handle);
+void sdrone_telemetry_send_data(sdrone_telemetry_handle_t telemetry_handle);
 
 #endif /* COMPONENTS_DRIVERS_TELEMETRY_INCLUDE_TELEMETRY_H_ */
--- telemetry/src/telemetry.c
+++ telemetry/src/telemetry.c
@@ -11,12 +11,132 @@
 #include <esp_http_server.h>
 #include <telemetry.h>
 
-#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
-#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+#define SDRONE_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define SDRONE_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define SDRONE_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 
 static const char *TAG = "sdrone_telemetry";
+static sdrone_telemetry_handle_t local_telemetry_handle = NULL;
+
+/***********************************************************************
+ ************************ W E B  S O C K E T ***************************
+ ***********************************************************************/
+static void ws_async_send(void *arg)
+{
+    sdrone_telemetry_handle_t telemetry_handle = arg;
+	if(telemetry_handle == NULL) {
+    	ESP_LOGE(TAG, "TELEMETRY_HANDLE NULL???");
+		return;
+	}
+
+    httpd_handle_t hd = telemetry_handle->sockets[telemetry_handle->cursor].hd;
+    int fd = telemetry_handle->sockets[telemetry_handle->cursor].fd;
+    httpd_ws_frame_t ws_pkt;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.payload = (uint8_t*)&telemetry_handle->data;
+    ws_pkt.len = sizeof(telemetry_handle->data);
+    ws_pkt.type = HTTPD_WS_TYPE_BINARY;
+    ws_pkt.final = true;
+
+    // send data
+	esp_err_t ret = httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+
+	// deregister socket (how to close it?)
+	if(ret != ESP_OK) {
+		telemetry_handle->sockets[telemetry_handle->cursor].hd = NULL;
+		telemetry_handle->sockets[telemetry_handle->cursor].fd = 0;
+	}
+}
+
+/***********************************************************************
+ ************************ W E B  S E R V E R ***************************
+ ***********************************************************************/
+static esp_err_t ws_get_handler(httpd_req_t *req)
+{
+	// register socket
+	sdrone_telemetry_handle_t telemetry_handle = local_telemetry_handle;
+	if(telemetry_handle == NULL) {
+    	ESP_LOGE(TAG, "TELEMETRY_HANDLE NULL???");
+		return ESP_FAIL;
+	}
+	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
+		if(telemetry_handle->sockets[i].hd == NULL) {
+			telemetry_handle->sockets[i].hd = req->handle;
+			telemetry_handle->sockets[i].fd = httpd_req_to_sockfd(req);
+			break;
+		}
+	}
+
+	// receive packet
+    httpd_ws_frame_t ws_pkt;
+	memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+	ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+
+	// determine the actual packet length
+    ws_pkt.payload = malloc(255); // extra byte for string NULL-termination
+    assert(ws_pkt.payload);
+
+    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 255);
+    if (ret != ESP_OK) {
+    	ESP_LOGE(TAG, "httpd_ws_recv_frame failed1: %s", esp_err_to_name(ret));
+        free(ws_pkt.payload);
+        return ret;
+    }
+
+    ESP_LOGI(TAG, "Websocket input is, type=%d, len=%d:", ws_pkt.type, ws_pkt.len);
+
+    free(ws_pkt.payload);
+
+    if (req->method == HTTP_GET) {
+        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
+        return ESP_OK;
+    }
+    return ESP_OK;
+}
+
+
+static esp_err_t wss_open_fd(httpd_handle_t hd, int sockfd)
+{
+    ESP_LOGI(TAG, "New client connected %d", sockfd);
+    return ESP_OK;
+}
+static void wss_close_fd(httpd_handle_t hd, int sockfd)
+{
+    ESP_LOGI(TAG, "Client disconnected %d", sockfd);
+}
+
+static httpd_handle_t start_webserver(sdrone_telemetry_handle_t telemetry_handle)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.open_fn = wss_open_fd;
+    config.close_fn = wss_close_fd;
+
+    httpd_uri_t ws_get = {
+            .uri        = "/ws",
+            .method     = HTTP_GET,
+            .handler    = ws_get_handler,
+            .user_ctx   = (void*)telemetry_handle,
+            .is_websocket = true
+    };
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &ws_get);
+        return server;
+    }
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+void stop_webserver(httpd_handle_t server) {
+	if (server != NULL) {
+		httpd_stop(server);
+	}
+}
 
 /***********************************************************************
  ****************************** W I F I ********************************
@@ -24,18 +144,23 @@ static const char *TAG = "sdrone_telemetry";
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
+	sdrone_telemetry_handle_t telemetry_handle = (sdrone_telemetry_handle_t)arg;
+
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
                  MAC2STR(event->mac), event->aid);
+        telemetry_handle->server = start_webserver(telemetry_handle);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
                  MAC2STR(event->mac), event->aid);
+        stop_webserver(telemetry_handle->server);
+        telemetry_handle->server = NULL;
     }
 }
 
-void wifi_init_softap(void)
+static void wifi_init_softap(sdrone_telemetry_handle_t telemetry_handle)
 {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -47,20 +172,20 @@ void wifi_init_softap(void)
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_event_handler,
-                                                        NULL,
+														(void*)telemetry_handle,
                                                         NULL));
 
     wifi_config_t wifi_config = {
         .ap = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
-            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .ssid = SDRONE_WIFI_SSID,
+            .ssid_len = strlen(SDRONE_WIFI_SSID),
+            .channel = SDRONE_WIFI_CHANNEL,
+            .password = SDRONE_WIFI_PASS,
+            .max_connection = SDRONE_MAX_STA_CONN,
             .authmode = WIFI_AUTH_WPA_WPA2_PSK
         },
     };
-    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+    if (strlen(SDRONE_WIFI_PASS) == 0) {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
 
@@ -69,6 +194,31 @@ void wifi_init_softap(void)
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+             SDRONE_WIFI_SSID, SDRONE_WIFI_PASS, SDRONE_WIFI_CHANNEL);
+}
+
+/***********************************************************************
+ ********************* P U B L I C  M E T H O D S **********************
+ ***********************************************************************/
+void sdrone_telemetry_init(sdrone_telemetry_handle_t telemetry_handle) {
+	local_telemetry_handle = telemetry_handle;
+	// init telemetry state
+	telemetry_handle->server = NULL;
+	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
+		telemetry_handle->sockets[i].fd = 0;
+		telemetry_handle->sockets[i].hd = NULL;
+	}
+
+	// init wifi AP
+    wifi_init_softap(telemetry_handle);
+    ESP_LOGI(TAG, "Telemetry Data Size: [%d]", sizeof(telemetry_handle->data));
+}
+void sdrone_telemetry_send_data(sdrone_telemetry_handle_t telemetry_handle) {
+	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
+		if(telemetry_handle->sockets[i].hd != NULL) {
+			telemetry_handle->cursor = i;
+			httpd_queue_work(telemetry_handle->sockets[i].hd, ws_async_send, telemetry_handle);
+		}
+	}
 }
 
