c17c04790fc0cb081c738df020884b658025c58a
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-11-22 09:41:46 +0100

    Aggiunto evento per i soli dati z

--- telemetry/include/telemetry.h
+++ telemetry/include/telemetry.h
@@ -14,17 +14,18 @@
 
 #define SDRONE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
 
-#define MESSAGE_RC      0x01
-#define MESSAGE_RPY     0x02
-#define MESSAGE_ACC     0x03
-#define MESSAGE_W       0x04
-#define MESSAGE_AXIS    0x05
-#define MESSAGE_GRAVITY 0x06
-#define MESSAGE_V       0x07
-#define MESSAGE_X       0x08
-#define MESSAGE_U       0x09
-#define MESSAGE_Y       0x0A
-#define NUM_MESSAGES    0x0A
+#define MESSAGE_RC            0x01
+#define MESSAGE_RPY           0x02
+#define MESSAGE_ACC           0x03
+#define MESSAGE_W             0x04
+#define MESSAGE_AXIS          0x05
+#define MESSAGE_GRAVITY       0x06
+#define MESSAGE_V             0x07
+#define MESSAGE_X             0x08
+#define MESSAGE_U             0x09
+#define MESSAGE_Y             0x0A
+#define MESSAGE_Z_DATA        0x0B
+#define NUM_MESSAGES          0x0B
 
 typedef struct {
     httpd_handle_t hd;
@@ -35,6 +36,14 @@ typedef struct {
 	uint16_t m_type;
 	uint32_t m_timestamp;
 	union {
+		struct {
+			int16_t rc_t;
+			float   x_t;
+			float   u_t;
+			float   w_t;
+			float   y_t;
+			float   vertical_v;
+		} z_data;
 		struct {
 			int16_t throttle;
 			int16_t roll;
