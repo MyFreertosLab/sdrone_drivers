83ea4154b70a19ca207653da2d7d6c305a1c78a8
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-12-21 19:24:02 +0100

    Aggiunto keep_alive e completato implementazione wsserver (quanto basta)

--- wsserver/include/wsserver.h
+++ wsserver/include/wsserver.h
@@ -11,22 +11,32 @@
 #include <esp_err.h>
 #include "esp_wifi.h"
 #include <esp_http_server.h>
+#include <keep_alive.h>
 
 #define SDRONE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+#define SDRONE_MAX_PUBLISHERS     2
+#define SDRONE_MAX_OBSERVERS      4
+
+typedef esp_err_t (*sdrone_wsserver_receive_frame_fn_t)(httpd_ws_frame_t* ws_pkt);
 
 typedef struct {
-    httpd_handle_t hd;
-    int fd;
-} sdrone_wsserver_gs_socket_t;
+	httpd_uri_t handler;
+	sdrone_wsserver_receive_frame_fn_t receive_cb;
+	int sockfd[SDRONE_MAX_OBSERVERS];
+} sdrone_httpd_uri_t;
+
+typedef sdrone_httpd_uri_t* sdrone_httpd_uri_handle_t;
 
 typedef struct {
 	httpd_handle_t server;
-	sdrone_wsserver_gs_socket_t sockets[SDRONE_MAX_STA_CONN];
-	uint8_t cursor;
+	wss_keep_alive_t keep_alive;
+	sdrone_httpd_uri_handle_t uri_handlers[SDRONE_MAX_PUBLISHERS];
 } sdrone_wsserver_state_t;
 typedef sdrone_wsserver_state_t* sdrone_wsserver_handle_t;
 
 void sdrone_wsserver_init(sdrone_wsserver_handle_t wsserver_handle);
-void sdrone_wsserver_send_data(sdrone_wsserver_handle_t wsserver_handle);
+void sdrone_wsserver_add_uri_handle(sdrone_httpd_uri_handle_t uri);
+void sdrone_wsserver_remove_uri_handle(sdrone_httpd_uri_handle_t uri);
+void sdrone_wsserver_send_data(sdrone_httpd_uri_handle_t uri_handler, httpd_ws_frame_t* frame);
 
 #endif /* COMPONENTS_DRIVERS_WSSERVER_INCLUDE_WSSERVER_H_ */
--- wsserver/src/wsserver.c
+++ wsserver/src/wsserver.c
@@ -10,131 +10,258 @@
 #include "lwip/sys.h"
 #include <esp_http_server.h>
 #include <wsserver.h>
+#include <keep_alive.h>
 
 #define SDRONE_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define SDRONE_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define SDRONE_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
 
 static const char *TAG = "sdrone_wsserver";
-static sdrone_wsserver_handle_t local_wsserver_handle = NULL;
+static sdrone_wsserver_handle_t internal_wsserver_handle = NULL;
+
+
+struct async_resp_arg {
+    httpd_handle_t hd;
+    int fd;
+};
 
 /***********************************************************************
  ************************ W E B  S O C K E T ***************************
  ***********************************************************************/
-static void ws_async_send(void *arg)
-{
-    sdrone_wsserver_handle_t wsserver_handle = arg;
-	if(wsserver_handle == NULL) {
-    	ESP_LOGE(TAG, "WSSERVER_HANDLE NULL???");
-		return;
-	}
 
-    httpd_handle_t hd = wsserver_handle->sockets[wsserver_handle->cursor].hd;
-    int fd = wsserver_handle->sockets[wsserver_handle->cursor].fd;
+static void send_ping(void *arg)
+{
+    struct async_resp_arg* resp_arg = arg;
+    httpd_handle_t hd = resp_arg->hd;
+    int fd = resp_arg->fd;
     httpd_ws_frame_t ws_pkt;
     memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-//    ws_pkt.payload = (uint8_t*)&wsserver_handle->data;
-//    ws_pkt.len = sizeof(wsserver_handle->data);
-    ws_pkt.type = HTTPD_WS_TYPE_BINARY;
-    ws_pkt.final = true;
+    ws_pkt.payload = NULL;
+    ws_pkt.len = 0;
+    ws_pkt.type = HTTPD_WS_TYPE_PING;
 
-    // send data
-	esp_err_t ret = httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+    httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+    free(resp_arg);
+}
 
-	// deregister socket (how to close it?)
-	if(ret != ESP_OK) {
-		wsserver_handle->sockets[wsserver_handle->cursor].hd = NULL;
-		wsserver_handle->sockets[wsserver_handle->cursor].fd = 0;
+static void send_pong(void *arg)
+{
+    struct async_resp_arg* resp_arg = arg;
+    httpd_handle_t hd = resp_arg->hd;
+    int fd = resp_arg->fd;
+    httpd_ws_frame_t ws_pkt;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.payload = NULL;
+    ws_pkt.len = 0;
+    ws_pkt.type = HTTPD_WS_TYPE_PONG;
+
+    httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+    free(resp_arg);
+}
+
+static esp_err_t sdrone_wsserver_add_client(sdrone_httpd_uri_handle_t uri_handle, int sockfd) {
+	for(uint8_t j = 0; j < SDRONE_MAX_OBSERVERS; j++) {
+		if(uri_handle->sockfd[j] == sockfd) {
+			return ESP_OK;
+		}
 	}
+	for(uint8_t j = 0; j < SDRONE_MAX_OBSERVERS; j++) {
+		if(uri_handle->sockfd[j] == -1) {
+			uri_handle->sockfd[j] = sockfd;
+	        ESP_LOGI(TAG, "ws_handler added a new socket [%d]", sockfd);
+	        return ESP_OK;
+		}
+	}
+	return ESP_FAIL;
+}
+static esp_err_t sdrone_ws_handler(httpd_req_t *req)
+{
+    sdrone_wsserver_handle_t wsserver_handle = internal_wsserver_handle;
+    int sockfd = httpd_req_to_sockfd(req);
+    sdrone_httpd_uri_handle_t uri_handle = NULL;
+
+    if (req->method == HTTP_GET) {
+        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
+        // add new socket for publisher
+        for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+        	if((wsserver_handle->uri_handlers[i] != NULL) && (strcmp(wsserver_handle->uri_handlers[i]->handler.uri, req->uri) == 0) && wsserver_handle->uri_handlers[i]->handler.method == HTTP_GET) {
+        		uri_handle = wsserver_handle->uri_handlers[i];
+        		return sdrone_wsserver_add_client(uri_handle, sockfd);
+        	}
+        }
+        ESP_LOGE(TAG, "ws_handler uri_handler not found for uri [%s]", req->uri);
+        return ESP_FAIL;
+    } else {
+    	// search publisher that publish for this socket
+        for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+        	for(uint8_t j = 0; j < SDRONE_MAX_OBSERVERS; j++) {
+        		if((wsserver_handle->uri_handlers[i] != NULL) && (wsserver_handle->uri_handlers[i]->sockfd[j] == sockfd)) {
+        			uri_handle = wsserver_handle->uri_handlers[i];
+        		}
+        	}
+        }
+        if(uri_handle == NULL) {
+            ESP_LOGE(TAG, "ws_handler uri_handler not found for uri [%s] with method [%d]", req->uri, req->method);
+        	return ESP_FAIL;
+        }
+    }
+
+    httpd_ws_frame_t ws_pkt;
+    uint8_t *buf = NULL;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+
+    // First receive the full ws message
+    /* Set max_len = 0 to get the frame len */
+    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "httpd_ws_recv_frame failed to get frame len with [%d]. type=[%d]", ret, ws_pkt.type);
+    }
+    ESP_LOGI(TAG, "frame len is %d and type [%d]", ws_pkt.len, ws_pkt.type);
+    if (ws_pkt.len > 0) {
+        /* ws_pkt.len + 1 is for NULL termination as we are expecting a string */
+        buf = calloc(1, ws_pkt.len + 1);
+        if (buf == NULL) {
+            ESP_LOGE(TAG, "Failed to calloc memory for buf");
+            return ESP_ERR_NO_MEM;
+        }
+        ws_pkt.payload = buf;
+        /* Set max_len = ws_pkt.len to get the frame payload */
+        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "httpd_ws_recv_frame failed with %d", ret);
+            free(buf);
+            return ret;
+        }
+    }
+    // If it was a PONG, update the keep-alive
+    if (ws_pkt.type == HTTPD_WS_TYPE_PONG) {
+        ESP_LOGI(TAG, "Received PONG message");
+        free(buf);
+        return wss_keep_alive_client_is_active(wsserver_handle->keep_alive, sockfd);
+    } else if (ws_pkt.type == HTTPD_WS_TYPE_PING) {
+            ESP_LOGI(TAG, "Received PING message");
+            free(buf);
+            struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+            resp_arg->hd = wsserver_handle->server;
+            resp_arg->fd = sockfd;
+            if (httpd_queue_work(wsserver_handle->server, send_pong, resp_arg) == ESP_OK) {
+                ESP_LOGI(TAG, "Sent PONG message");
+            } else {
+                ESP_LOGW(TAG, "Failed to send PONG message");
+            }
+
+
+    } else {
+        ESP_LOGI(TAG, "Received packet with message: %s", ws_pkt.payload);
+        esp_err_t ret = ESP_OK;
+        if(uri_handle->receive_cb != NULL) {
+            ret = uri_handle->receive_cb(&ws_pkt);
+        }
+        free(buf);
+        return ret;
+    }
+    free(buf);
+    return ESP_OK;
 }
 
 /***********************************************************************
  ************************ W E B  S E R V E R ***************************
  ***********************************************************************/
-static esp_err_t ws_get_handler(httpd_req_t *req)
-{
-	// register socket
-	sdrone_wsserver_handle_t wsserver_handle = local_wsserver_handle;
-	if(wsserver_handle == NULL) {
-    	ESP_LOGE(TAG, "WSSERVER_HANDLE NULL???");
-		return ESP_FAIL;
-	}
-	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
-		if(wsserver_handle->sockets[i].hd == NULL) {
-			wsserver_handle->sockets[i].hd = req->handle;
-			wsserver_handle->sockets[i].fd = httpd_req_to_sockfd(req);
-			break;
-		}
-	}
-
-	// receive packet
-    httpd_ws_frame_t ws_pkt;
-	memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-	ws_pkt.type = HTTPD_WS_TYPE_TEXT;
-
-	// determine the actual packet length
-    ws_pkt.payload = malloc(255); // extra byte for string NULL-termination
-    assert(ws_pkt.payload);
-
-    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 255);
-    if (ret != ESP_OK) {
-    	ESP_LOGE(TAG, "httpd_ws_recv_frame failed1: %s", esp_err_to_name(ret));
-        free(ws_pkt.payload);
-        return ret;
-    }
-
-    ESP_LOGI(TAG, "Websocket input is, type=%d, len=%d:", ws_pkt.type, ws_pkt.len);
-
-    free(ws_pkt.payload);
-
-    if (req->method == HTTP_GET) {
-        ESP_LOGI(TAG, "Handshake done, the new connection was opened");
-        return ESP_OK;
-    }
-    return ESP_OK;
-}
-
-
 static esp_err_t wss_open_fd(httpd_handle_t hd, int sockfd)
 {
     ESP_LOGI(TAG, "New client connected %d", sockfd);
-    return ESP_OK;
+    sdrone_wsserver_handle_t wsserver_handle = httpd_get_global_user_ctx(hd);
+    return wss_keep_alive_add_client(wsserver_handle->keep_alive, sockfd);
 }
 static void wss_close_fd(httpd_handle_t hd, int sockfd)
 {
     ESP_LOGI(TAG, "Client disconnected %d", sockfd);
+    sdrone_wsserver_handle_t wsserver_handle = httpd_get_global_user_ctx(hd);
+	for (uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+		if(internal_wsserver_handle->uri_handlers[i] != NULL) {
+			for (uint8_t j = 0; j < SDRONE_MAX_OBSERVERS; j++) {
+				if (internal_wsserver_handle->uri_handlers[i]->sockfd[j] == sockfd) {
+					internal_wsserver_handle->uri_handlers[i]->sockfd[j] = -1;
+				}
+			}
+		}
+	}
+    wss_keep_alive_remove_client(wsserver_handle->keep_alive, sockfd);
 }
 
-static httpd_handle_t start_webserver(sdrone_wsserver_handle_t wsserver_handle)
+static void user_global_ctx_free(void* ctx) {
+    ESP_LOGI(TAG, "Free user global context");
+    sdrone_wsserver_handle_t wsserver_handle = (sdrone_wsserver_handle_t)ctx;
+	wsserver_handle->server = NULL;
+}
+static bool client_not_alive_cb(wss_keep_alive_t h, int fd)
 {
+    ESP_LOGE(TAG, "Client not alive, closing fd %d", fd);
+    sdrone_wsserver_handle_t wsserver_handle = (sdrone_wsserver_handle_t)wss_keep_alive_get_user_ctx(h);
+    httpd_sess_trigger_close(wsserver_handle->server, fd);
+    return true;
+}
+
+static bool check_client_alive_cb(wss_keep_alive_t h, int fd)
+{
+    ESP_LOGI(TAG, "Checking if client (fd=%d) is alive", fd);
+    struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+    sdrone_wsserver_handle_t wsserver_handle = (sdrone_wsserver_handle_t)wss_keep_alive_get_user_ctx(h);
+    resp_arg->hd = wsserver_handle->server;
+    resp_arg->fd = fd;
+
+    if (httpd_queue_work(resp_arg->hd, send_ping, resp_arg) == ESP_OK) {
+        return true;
+    }
+    ESP_LOGI(TAG, "Checking return false");
+    return false;
+}
+
+static esp_err_t start_webserver(sdrone_wsserver_handle_t wsserver_handle)
+{
+    // Prepare keep-alive engine
+    wss_keep_alive_config_t keep_alive_config = KEEP_ALIVE_CONFIG_DEFAULT();
+    keep_alive_config.max_clients = SDRONE_MAX_OBSERVERS;
+    keep_alive_config.client_not_alive_cb = client_not_alive_cb;
+    keep_alive_config.check_client_alive_cb = check_client_alive_cb;
+    keep_alive_config.user_ctx = (void*)wsserver_handle;
+    keep_alive_config.keep_alive_period_ms = 30000;
+    keep_alive_config.not_alive_after_ms = 50000;
+    wss_keep_alive_t keep_alive = wss_keep_alive_start(&keep_alive_config);
+    wsserver_handle->keep_alive = keep_alive;
+
+    // Prepare Httpd server
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.open_fn = wss_open_fd;
     config.close_fn = wss_close_fd;
-
-    httpd_uri_t ws_get = {
-            .uri        = "/ws",
-            .method     = HTTP_GET,
-            .handler    = ws_get_handler,
-            .user_ctx   = (void*)wsserver_handle,
-            .is_websocket = true
-    };
+    config.global_user_ctx = (void*)wsserver_handle;
+    config.global_user_ctx_free_fn = user_global_ctx_free;
+    config.core_id = 1;
+    config.send_wait_timeout = 20;
+    config.recv_wait_timeout = 20;
 
     // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
-        // Set URI handlers
-        ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &ws_get);
-        return server;
+    	wsserver_handle->server = server;
+    	// register uri handlers
+    	for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+    		if(wsserver_handle->uri_handlers[i] != NULL) {
+    			httpd_register_uri_handler(wsserver_handle->server, &wsserver_handle->uri_handlers[i]->handler);
+    			ESP_LOGI(TAG, "uri handler [%s] registered", wsserver_handle->uri_handlers[i]->handler.uri);
+    		}
+    	}
+        return ESP_OK;
     }
-    ESP_LOGI(TAG, "Error starting server!");
-    return NULL;
+    ESP_LOGE(TAG, "Error starting server!");
+    return ESP_FAIL;
 }
 
-void stop_webserver(httpd_handle_t server) {
-	if (server != NULL) {
-		httpd_stop(server);
+static void stop_webserver(sdrone_wsserver_handle_t wsserver_handle) {
+	if (wsserver_handle->server != NULL) {
+	    wss_keep_alive_stop(wsserver_handle->keep_alive);
+		httpd_stop(wsserver_handle->server);
 	}
 }
 
@@ -150,13 +277,12 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
                  MAC2STR(event->mac), event->aid);
-        wsserver_handle->server = start_webserver(wsserver_handle);
+        start_webserver(wsserver_handle);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
                  MAC2STR(event->mac), event->aid);
-        stop_webserver(wsserver_handle->server);
-        wsserver_handle->server = NULL;
+        stop_webserver(wsserver_handle);
     }
 }
 
@@ -200,23 +326,68 @@ static void wifi_init_softap(sdrone_wsserver_handle_t wsserver_handle)
 /***********************************************************************
  ********************* P U B L I C  M E T H O D S **********************
  ***********************************************************************/
+
 void sdrone_wsserver_init(sdrone_wsserver_handle_t wsserver_handle) {
-	local_wsserver_handle = wsserver_handle;
+
+	//
+	internal_wsserver_handle = wsserver_handle;
+
 	// init wsserver state
 	wsserver_handle->server = NULL;
-	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
-		wsserver_handle->sockets[i].fd = 0;
-		wsserver_handle->sockets[i].hd = NULL;
+	for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+		wsserver_handle->uri_handlers[i] = NULL;
 	}
 
 	// init wifi AP
     wifi_init_softap(wsserver_handle);
 }
-void sdrone_wsserver_send_data(sdrone_wsserver_handle_t wsserver_handle) {
-	for(uint8_t i = 0; i < SDRONE_MAX_STA_CONN; i++) {
-		if(wsserver_handle->sockets[i].hd != NULL) {
-			wsserver_handle->cursor = i;
-			httpd_queue_work(wsserver_handle->sockets[i].hd, ws_async_send, wsserver_handle);
+
+void sdrone_wsserver_add_uri_handle(sdrone_httpd_uri_handle_t uri) {
+	uint8_t pos = 255;
+	for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+		if(internal_wsserver_handle->uri_handlers[i] == NULL && pos == 255) {
+			pos = i;
+		} else if(internal_wsserver_handle->uri_handlers[i] == uri) {
+			ESP_LOGE(TAG, "Uri handler already exists for uri [%s]! Not added", uri->handler.uri);
+			return;
+		}
+	}
+	internal_wsserver_handle->uri_handlers[pos] = uri;
+
+	// init sockets for this handler
+	for(uint8_t i = 0; i < SDRONE_MAX_OBSERVERS; i++) {
+		uri->sockfd[i] = -1;
+	}
+
+	// set default handler callback if it's NULL
+	if(uri->handler.handler == NULL) {
+		uri->handler.handler  = sdrone_ws_handler;
+	}
+
+	ESP_LOGI(TAG, "uri handler [%s] added at position %d", uri->handler.uri, pos);
+	if(internal_wsserver_handle->server != NULL) {
+		httpd_register_uri_handler(internal_wsserver_handle->server, &uri->handler);
+		ESP_LOGI(TAG, "uri handler [%s] registered", uri->handler.uri);
+	}
+}
+
+void sdrone_wsserver_remove_uri_handle(sdrone_httpd_uri_handle_t uri) {
+	httpd_unregister_uri_handler(internal_wsserver_handle->server, uri->handler.uri, uri->handler.method);
+	for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+		if(internal_wsserver_handle->uri_handlers[i] == uri) {
+			internal_wsserver_handle->uri_handlers[i] = NULL;
+		}
+	}
+}
+
+void sdrone_wsserver_send_data(sdrone_httpd_uri_handle_t uri_handler, httpd_ws_frame_t* frame) {
+	for(uint8_t i = 0; i < SDRONE_MAX_PUBLISHERS; i++) {
+		if(internal_wsserver_handle->uri_handlers[i] == uri_handler) {
+			for(uint8_t j = 0; j < SDRONE_MAX_OBSERVERS; j++) {
+				if(internal_wsserver_handle->uri_handlers[i]->sockfd[j] >= 0) {
+					httpd_ws_send_frame_async(internal_wsserver_handle->server, internal_wsserver_handle->uri_handlers[i]->sockfd[j], frame);
+				}
+			}
 		}
 	}
 }
